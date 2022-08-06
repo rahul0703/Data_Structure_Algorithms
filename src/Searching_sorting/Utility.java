142f249e4cd095123447e89bf1d8785a5a9decb8
Rahul.Sureka <rahulsureka516@gmail.com>
2022-08-06 15:41:32 +0530

    Project Creation

--- src/Searching_sorting/Utility.java
+++ src/Searching_sorting/Utility.java
@@ -1,4 +1,37 @@
 package Searching_sorting;
 
 public class Utility {
+
+    public int majorityElement(int[] nums) {
+        Integer num = null;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            int currNum = nums[i];
+            if(num == null){
+                num = currNum;
+                count++;
+            }else{
+                if(currNum == num){
+                    count++;
+                }else{
+                    count--;
+                    if(count <= 0){
+                        num = currNum;
+                        count = 1;
+                    }
+                }
+            }
+        }
+
+        int finalCount = 0;
+        for(int currNum2 : nums){
+            if(currNum2 == num){
+                finalCount++;
+            }
+        }
+        if(finalCount > nums.length/2){
+            return num;
+        }
+        return -1;
+    }
 }
