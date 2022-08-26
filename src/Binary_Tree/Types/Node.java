830db108a5f61ed8ad916ebbd279619940787fc9
Rahul.Sureka <rahulsureka516@gmail.com>
2022-08-27 03:49:07 +0530

    clone tree with random node

---
+++ src/Binary_Tree/Types/Node.java
@@ -0,0 +1,41 @@
+package Binary_Tree.Types;
+
+public class Node {
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int val){
+        this.val = val;
+    }
+
+    public Node(int val, Node left, Node right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
